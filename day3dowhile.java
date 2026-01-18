43b2c46ee1b792cac20ebeeba82ede19e96984d1
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-19 00:22:26 +0530

    do-while loop logic understanding

---
+++ day3dowhile.java
@@ -0,0 +1,17 @@
+public class day3dowhile {
+    public static void main(String args[]) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i > 10);
+        System.out.println("-------------");
+        int j = 5;
+        do {
+            System.out.println(j);
+            j++;
+
+        } while (j <= 10);
+
+    }
+}
