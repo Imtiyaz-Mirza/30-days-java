ef8a4081e11de531c4bf92e9b52fb04e77df3092
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-18 23:20:36 +0530

    while loop understanding

---
+++ day3whileloop.java
@@ -0,0 +1,15 @@
+public class day3whileloop {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+        int j = 5;
+        while (j >= 1) {
+            System.out.println(j);
+            j--;
+
+        }
+    }
+}
