72e61c3a5b7d816a5c76d74ea9a0382516be7428
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-22 14:50:18 +0530

    Arrays concept

---
+++ day4array.java
@@ -0,0 +1,8 @@
+public class day4array {
+    public static void main(String args[]) {
+        String[] names = { "Jay", "Priya", "Sam" };
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("The names are : " + (i + 1) + ". " + names[i]);
+        }
+    }
+}
