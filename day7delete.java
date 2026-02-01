05dbe3a1d606dc1ff9122e72426cd0080c3a2a11
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-02-02 00:04:21 +0530

    small update

--- day7delete.java
+++ day7delete.java
@@ -7,6 +7,19 @@ public class day7delete {
         Scanner userInput = new Scanner(System.in);
         list.add("learn");
         list.add("delete");
-
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+        System.out.print("Enter task number to delete : ");
+        int usernum = userInput.nextInt();
+        userInput.nextLine();
+        int index = usernum - 1;
+        if (index >= 0 && index < list.size()) {
+            list.remove(index);
+            System.out.println("Task deleted !");
+        } else {
+            System.out.println("Invalid task number !");
+        }
     }
+
 }
