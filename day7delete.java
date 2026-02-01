91a2f988fc4bf5ac850b22f6b338594683158319
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-02-01 23:58:23 +0530

    Delete logic

---
+++ day7delete.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class day7delete {
+    public static void main(String args[]) {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner userInput = new Scanner(System.in);
+        list.add("learn");
+        list.add("delete");
+
+    }
+}
